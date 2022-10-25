30c89787cd2063217d75d0333943126d73098e42
Abrar-eng <abrarulhasan600@gmail.com>
2022-10-26 00:29:36 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC0zERqJCuxR_jHC3RHlyAh4k1-hc-eOuM",
+  authDomain: "abrar-learning-school.firebaseapp.com",
+  projectId: "abrar-learning-school",
+  storageBucket: "abrar-learning-school.appspot.com",
+  messagingSenderId: "565721158412",
+  appId: "1:565721158412:web:2524ab5d1c89a1ac8f6f29"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
